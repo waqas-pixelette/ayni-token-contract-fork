29ae1c3c17293c04bac0267bd19c0f3b38ec7cb1
lukmaan-k <lukmaankolia@gmail.com>
2025-05-05 16:04:30 +0100

    files for token audit

--- tasks/blocknumber.ts
+++
@@ -1,6 +0,0 @@
-import { task } from "hardhat/config";
-
-task("blocknumber", "Prints the block number", async (_, { ethers }) => {
-  const blockNumber = await ethers.provider.getBlockNumber();
-  console.log(blockNumber);
-});
